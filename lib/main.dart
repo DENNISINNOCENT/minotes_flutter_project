4fa3f39d9ccfb2d53b15a9581f85d4294d72c42b
DENNISINNOCENT <mutindahdennis@gmail.com>
2023-02-16 14:58:39 +0300

    user verification and login authentication

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:minotes/views/login_view.dart';
-import 'package:minotes/views/register_view.dart';
 
 import 'firebase_options.dart';
+import 'views/login_view.dart';
+import 'views/register_view.dart';
+import 'views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,14 +38,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              // print(user);
-              // if (user?.emailVerified ?? false) {
-              //   return const Text('Done');
-              // } else {
-              //   return const VerifyEmailView();
-              // }
-              return const LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                if (user.emailVerified) {
+                  print('Email is verified');
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+              return const Text('Done');
 
             default:
               return const Text('loading');
